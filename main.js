74cbd0b46eabc6e7da91e6c1f8726d103c203a69
Sarah Ciston <ciston@usc.edu>
2023-03-31 15:39:56 -0700

    auto cycle all choices

--- main.js
+++ main.js
@@ -1,43 +1,17 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
-
 import { ProsePlay} from "proseplay"
 
+const data = [] //import data TK
 
-
-let doc1 = `this is the (hello|world)[1]
-here is another (line|question->turnLight|answer->turnDark)[1]`;
-
-let doc2 = 'here is another (text|series)[-]';
-
-let docs = [doc1, doc2]
-
-// ProsePlay.parse(doc1);
+let doc1 = `this is the (hello|world|you|like|to|talk)
+here is another (line|question->turnLight|answer->turnDark|welcome|again)`;
+// let doc2 = 'here is another (text|series)[-]';
+// let docs = [doc1, doc2]
+let docs = [doc1]
 
 docs.forEach((count, i) => {
     const el = document.createElement('div')
+    el.classList.add("databox")
     document.body.appendChild(el)
     
     const pp = new ProsePlay(el)
@@ -47,18 +21,77 @@ docs.forEach((count, i) => {
     pp.setFunction("turnLight", turnLight);
     pp.setFunction("turnDark", turnDark);
 
-    
+    // let w = pp.windowIndex
+    // timer(w)
+
+    setInterval(multi, 2000)
+
+    function single(){
+        let len = pp.choices[0].length
+        let current = pp.currentIndexes[0]
+        let next = current+1
+        console.log(len, current)
+
+        if (next > len-1){
+            pp.currentIndexes[0] = -1
+            next = 0
+            pp.slideWindow(0,next)
+        } 
+        pp.slideWindow(0,next)
+    }
+
+    function multi(){
+        pp.choices.forEach((list, choice)=>{
+            let len = list.length
+            let current = pp.currentIndexes[choice]
+            let next = current+1
+            console.log(len, current)
+
+            if (next> len-1){
+                pp.currentIndexes[choice] = -1
+                next = 0
+                // pp.slideWindow(choice,next)
+            }
+            pp.slideWindow(choice,next)
+        })
+    }
+        // pp.currentIndexes.forEach((window, index)=>{
+        //     console.log(window, index)
+        //     pp.slideWindow(window,index++)
+        // })
+        // pp.currentIndexes.forEach((w) => {
+        //     console.log("choice: ", w)
+        //     pp.slideWindow(w, w++)
+        // })
+        
+        // pp.slideWindow(pp.currentIndex, pp.currentIndex++)
+
+
+    // pp.setFunction("changeAll", )
+
+    // function test(){
+    //     let len = pp.choices.length
+    //     console.log(len)
+
+    //     for (let w = 0; w < len-1; w++){
+    //         pp.slideWindow(w, w++)
+    //     }
+
+        // pp.choices.forEach((w) => {
+            
+        // })
+
+        // for (let w, w < len-1, w++){
+        //     pp.slideWindow(w,w++)
+        // }
+        // let i = pp.choices.currentIndex
+        // console.log(i)
+        // pp.slideWindow(i, i++)
+        // pp.slideWindow(0,1)
+        // console.log('moved')
+    // }
+
 })
-
-// const containers = document.querySelectorAll('.text')
-// containers.forEach((container, i) => {
-//     const pp = new ProsePlay(container)
-//     pp.parse(docs[i])
-// })
-
-// const pp = new ProsePlay(containers)
-// pp.classList.add('.proseplay-plaintext')
-// pp.parse(doc1)
 
 function turnLight() {
     document.body.classList.remove("dark");
@@ -68,5 +101,14 @@ function turnLight() {
 function turnDark(pp) {
     document.body.classList.remove("light");
     document.body.classList.add("dark");
+}
+
+function timer(w){
+    setInterval(slider(w), 1000)
+}
+
+function slider(i){
+    // pp.slideWindow(i, i++)
+    pass
 }
 
